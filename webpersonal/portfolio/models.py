84a8bc47fc428bdc988999ee66b56ee91f4fb52b
AlanAlvarez5 <aa.alvarezsanchez@ugto.mx>
2019-02-20 20:52:14 -0600

    Proyecto añadido

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length = 200, verbose_name = "Titulo")
+    description = models.TextField(verbose_name = "Descripción")
+    image = models.ImageField(verbose_name = "Imagen", upload_to="projects")
+    created = models.DateTimeField(auto_now_add = True, verbose_name = "Fecha de Creación")
+    updated = models.DateTimeField(auto_now = True, verbose_name = "Fecha de Edición")
+    url = models.URLField(verbose_name = "Dirección Web", null = True, blank = True)
+
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.title
